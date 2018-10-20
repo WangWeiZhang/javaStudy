933e226d40bdf4ad320ac64f82e811f4626738ef
wangwz7 <wangwz7@lenovo.com>
2018-10-20 17:50:16 +0800

    java study
    第一章和第二章笔记小程序

---
+++ demo/MyFirstJava.java
@@ -0,0 +1,31 @@
+/**
+ * Description：这是我的第一个java程序<br>
+ * Author：王维璋<br>
+ * Create Date:2018-9-15<br>
+ * Modified Date:2018-10-3<br>
+ * Modified By:Mr Sun<br>
+ */
+public class MyFirstJava{
+	/**
+	 * Description：这是java程序的入口<br>
+	 * 参数：字符串数组<br>
+	 */
+	public static void main(String []args) {
+		//打印输出到控制台
+		System.out.println("hello world!");
+		// \n 转意符号 换行
+		System.out.print("hello world!\n");
+		System.out.println("hello world!");
+		// \t 转意符号 tab 空格
+		System.out.println("hello wor\tld");
+	}
+	
+	/**
+	 * Description：显示输出个人的信息<br>
+	 * @return String 姓名<br>
+	 * @param content 个人资料<br>
+	 */
+	public String toString(String content) {
+		return "";
+	}
+}
